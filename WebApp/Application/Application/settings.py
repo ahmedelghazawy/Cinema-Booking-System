8bed48ed5619ef8bcc3363a3876807d2f6aa98ad
Michal Wesolowski <sc16maw@leeds.ac.uk>
2018-02-14 14:57:09 +0000

    Implemented index page as a django template

--- WebApp/Application/Application/settings.py
+++ WebApp/Application/Application/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Application.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'source/Templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
