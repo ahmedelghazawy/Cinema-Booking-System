7ccfd25ff79e2420808654b59ad6bbbeb4e88417
Michal Wesolowski <sc16maw@leeds.ac.uk>
2018-02-16 13:50:49 +0000

    Fixed static files settings

--- WebApp/Application/Application/settings.py
+++ WebApp/Application/Application/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = "Static_media/"
-STATIC_URL = "Static/"
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = "/Static/"
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "Static_deploy")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "Static")]
