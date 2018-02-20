441780e9ea84dd625c2f67d96ab1542f85260526
Ahmed El Ghazawy <sc16ameg@leeds.ac.uk>
2018-02-20 14:50:06 +0000

    new branch with the proper data for all requirements

--- TillApp/src/Movie.java
+++ TillApp/src/Movie.java
@@ -1,4 +1,8 @@
 import java.util.List;
+import java.lang.*;
+import java.sql.Timestamp;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 /**
  * Created by sc16tdad on 09/02/18.
@@ -6,6 +10,7 @@ import java.util.List;
 public class Movie {
     private String title;
     private String certificate;
+    private Timestamp duration;
     private List<Screening> screenings;
 
     public String getTitle() {
@@ -14,6 +19,33 @@ public class Movie {
 
     public String getCertificate() { return certificate; }
 
+    public Timestamp getDuration(){ return duration; }
+
     @Override
     public String toString() { return title; }
+}
+
+class MovieData {
+
+    public static void queryTitle(Sql2o database)
+    {
+
+        System.out.println("\nTitle:\n");
+
+        try (Connection conn = database.open())
+        {
+            List<Movie> movies = conn.createQuery("SELECT * FROM Movie").executeAndFetch(Movie.class);
+
+            for (Movie movie: movies)
+            {
+                System.out.println(movie.toString());
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+      Sql2o db = new Sql2o("jdbc:sqlite:../../WebApp/Application/databse.db", "", "");
+      queryTitle(db);
+    }
 }
