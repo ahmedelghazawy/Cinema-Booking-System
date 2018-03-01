c8cd7f3cb16e6e06b99eba63650e4163d9e0f7a4
Abdul Hasnat <ll15a9h@leeds.ac.uk>
2018-03-01 02:53:46 +0000

    Index page testing

--- WebApp/Application/source/tests.py
+++ WebApp/Application/source/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+import os
+import unittest
+import tempfile
+from app import *
+from app.models import *
 
 # Create your tests here.
+
+    # testing index page loads
+    def test_index(self):
+        response = self.app.get('/', follow_redirects=True, content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+
+    # tests index page is correct
+    def test_loginPageLoads(self):
+        response = self.app.get('/index', follow_redirects=True)
+        self.assertIn(b'New Releases',response.data)
+
+# commit
