52fc431619ceba36b1545d8309de1f80ba9583f7
Abdul Hasnat <ll15a9h@leeds.ac.uk>
2018-03-01 03:54:38 +0000

    Index page testing pt2

--- WebApp/Application/source/tests.py
+++ WebApp/Application/source/tests.py
@@ -1,12 +1,10 @@
-from django.test import TestCase
 import os
 import unittest
 import tempfile
-from app import *
-from app.models import *
 
-# Create your tests here.
 
+class Tests(unittest.TestCase):
+    # Create your tests here.
     # testing index page loads
     def test_index(self):
         response = self.app.get('/', follow_redirects=True, content_type='html/text')
@@ -14,8 +12,20 @@ from app.models import *
 
 
     # tests index page is correct
-    def test_loginPageLoads(self):
+    def test_home_text1(self):
         response = self.app.get('/index', follow_redirects=True)
         self.assertIn(b'New Releases',response.data)
 
-# commit
+    def test_home_text2(self):
+        response = self.app.get('/index', follow_redirects=True)
+        self.assertIn(b'New Releases',response.data)
+
+
+    # testing whats page loads
+    def test_whatson(self):
+        response = self.app.get('/whatson', follow_redirects=True, content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    # main
+if __name__ == '__main__':
+    unittest.main()
