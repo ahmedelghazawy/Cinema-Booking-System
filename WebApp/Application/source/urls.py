2b697976dc6dbceae372971bfbc155dca96aa4a3
Ali Ammar <sc16aa@leeds.ac.uk>
2018-03-01 19:32:17 +0000

    added urls for api pages to display movies and individual movie details

--- WebApp/Application/source/urls.py
+++ WebApp/Application/source/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    url(r'^test2$', views.test2, name = 'test2'),
     url(r'^whatson$', views.whatson, name = 'whatson'),
+    url(r'^api/whatsonapi$', views.whatsonapi.as_view()),
+    url(r'^api/movieTimingsapi/(?P<MovieID>\d+)/(?P<date>[\w\-]+)/$', views.movieTimingsapi.as_view()),
+    #url(r'^test2$', views.test2.as_view()),
     url(r'^moviePage/(?P<MovieID>\d+)/$', views.moviePage, name = 'moviePage'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
