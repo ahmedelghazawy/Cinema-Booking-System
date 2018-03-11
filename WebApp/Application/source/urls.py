28895ac611f1b99fd861b1e3f5344689e6010e57
Ali Ammar <sc16aa@leeds.ac.uk>
2018-03-11 13:08:10 +0000

    user login and registration functionality implemented

--- WebApp/Application/source/urls.py
+++ WebApp/Application/source/urls.py
@@ -5,10 +5,14 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^whatson$', views.whatson, name = 'whatson'),
+    url(r'^login$', views.loginPage, name = 'loginPage'),
+    url(r'^logout$', views.logoutPage, name = 'logoutPage'),
+    url(r'^register$', views.registerPage, name = 'registerPage'),
     url(r'^api/whatsonapi$', views.whatsonapi.as_view()),
     url(r'^api/movieTimingsapi/(?P<MovieID>\d+)/(?P<date>[\w\-]+)/$', views.movieTimingsapi.as_view()),
     #url(r'^test2$', views.test2.as_view()),
     url(r'^moviePage/(?P<MovieID>\d+)/$', views.moviePage, name = 'moviePage'),
+    url(r'^booking/(?P<ScreeningID>\d+)/$', views.bookingPage, name = 'bookingPage'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- WebApp/Application/source/views.py
+++ WebApp/Application/source/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponse
 from django.db import models
 from source.models import *
+from source.forms import *
 from source.serializers import *
 from datetime import datetime, time
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, get_user_model, logout
 # Create your views here.
 def index(request):
 	latest_movies = Movie.objects.order_by('-releaseDate')[:4]
@@ -17,6 +19,36 @@ def whatson(request):
 	movies = Movie.objects.all()
 	return render(request,'whatson.html',{'nbar':'whatson','movies':movies} )
 
+def loginPage(request):
+	title="login"
+	form = UserLoginForm(request.POST or None)
+	if form.is_valid():
+		username = form.cleaned_data.get('username')
+		password = form.cleaned_data.get('password')
+		user = authenticate(username = username, password = password)
+		login(request, user)
+		return redirect("/")
+
+	return render(request,'login.html',{'title':title,'form':form} )
+
+def registerPage(request):
+	title ="register"
+	form = UserRegisterForm(request.POST or None)
+	if form.is_valid():
+		user = form.save(commit = False)
+		password = form.cleaned_data.get('password')
+		user.set_password(password)
+		user.save()
+		new_user = authenticate(username = user.username, password = password)
+		login(request, new_user)
+		return redirect("/")
+
+
+	return render(request,'login.html',{'title':title,'form':form} )
+def logoutPage(request):
+	logout(request)
+	return render(request,'login.html',{} )
+
 def moviePage(request, MovieID):
 	movie = Movie.objects.filter(id=MovieID).first()
 	currentDateTime = datetime.now()
@@ -25,9 +57,14 @@ def moviePage(request, MovieID):
 	latest_movies = Movie.objects.order_by('-releaseDate')[:4]
 	return render(request,'movieTile.html',{'movie':movie, 'timings':timings, 'currentTime':currentTime, 'latest_movies':latest_movies} )
 
-def BookingPage(request):
-	movies = Movie.objects.all()
-	return render(request,'whatson.html',{'nbar':'whatson','movies':movies} )
+def bookingPage(request, ScreeningID):
+	screening = Screening.objects.filter(id = ScreeningID).first()
+	temp = screening.screen_id.id
+	screen = Screen.objects.filter(id = temp).first()
+	totalSeats = screen.standardSeats
+	#emptySeats =
+	seats = Seat.objects.filter(screening_id = ScreeningID).all().count()
+	return render(request,'booking.html',{'nbar':'whatson','seats':seats, 'totalSeats':totalSeats} )
 
 class whatsonapi(APIView):
 	def get(self, request):
