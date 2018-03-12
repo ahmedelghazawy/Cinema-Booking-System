6801e42581a3711c88f8333a3e0f928282895c0d
Ahmed Elghazawy <sc16ameg@leeds.ac.uk>
2018-03-12 19:32:16 +0000

    established a connection and a basic hardcoded get request to the api

--- TillApp/src/APIConnection.java
+++ TillApp/src/APIConnection.java
@@ -1,15 +1,39 @@
 import java.net.*;
+import java.io.*;
 
 public class APIConnection{
-  public void get() throws Exception{
-    URL url = new URL ("127.0.0.1:8000/api/movieTimingsapi/1/2018-03-12");
+  public static void get() throws Exception{
+    URL url = new URL ("http://127.0.0.1:8000/api/movieTimingsapi/1/2018-03-12/?format=json");
     HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    
+
 
     con.setRequestMethod("GET");
+    con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+    int responseCode = con.getResponseCode();
+    System.out.println("response code = " + responseCode);
+
+    BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
   }
 
   public static void main(String[] args) {
+    try {
 
+    APIConnection.get();
+    }
+    catch(Exception e){
+      System.out.println(e);
+    }
   }
 }
