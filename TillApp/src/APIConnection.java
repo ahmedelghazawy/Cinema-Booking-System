bafb4d3cac3758e4534d69b8e6d8fbc4a51176b9
Thayna Dorneles <sc16tdad@leeds.ac.uk>
2018-03-20 18:47:00 +0000

    Using Jackson API instead of Gson API, however, still getting error when running it

--- TillApp/src/APIConnection.java
+++ TillApp/src/APIConnection.java
@@ -1,7 +1,9 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import org.codehaus.jackson.map.ObjectMapper;
 import java.net.*;
 import java.io.*;
+import java.util.List;
+import java.sql.Timestamp;
+
 
 public class APIConnection{
     public static void get() throws Exception
@@ -38,14 +40,37 @@ public class APIConnection{
             // APIConnection.get();
             // String json = "{\"id\":1,\"title\":\"The Greates Showman\",\"cover\":\"img/MovieCovers/TheGreatestShowman.jpg\",\"rating\":8.0,\"certificate\":\"0\",\"releaseDate\":\"2018-02-18\"}";
             String json = "{\"id\":1, \"title\": \"hello\"}";
-            Gson gson = new Gson();
-            Movie movie = gson.fromJson(json, Movie.class);
+            // Gson gson = new Gson();
+            // Movie movie = gson.fromJson(json, Movie.class);
             // Movie movie = new Movie(3, "Hey There!");
             // System.out.println(movie.getTitle());
+            ObjectMapper mapper = new ObjectMapper();
+            Movie movie = mapper.readValue(json, Movie.class);
+            System.out.println(movie.getTitle());
         }
         catch(Exception e)
         {
             System.out.println(e);
         }
     }
+}
+
+
+// Small version of Movie for testing
+class Movie {
+    private String title = "";
+    private int id = 0;
+
+    public Movie(){
+        this.id = 0;
+        this.title = null;
+    }
+
+    public Movie(int id, String title)
+    {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getTitle() { return title; }
 }
