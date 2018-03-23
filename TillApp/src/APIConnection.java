9126e704ee4e3d8f887ed4da9cf14e9a5b6898e9
Thayna Dorneles <sc16tdad@leeds.ac.uk>
2018-03-23 17:48:46 +0000

    Main class creating Movie objects from API

--- TillApp/src/APIConnection.java
+++ TillApp/src/APIConnection.java
@@ -1,12 +1,11 @@
 import org.codehaus.jackson.map.ObjectMapper;
 import java.net.*;
 import java.io.*;
-import java.util.List;
-import java.sql.Timestamp;
-
+import java.util.*;
 
 public class APIConnection{
-    public static void get() throws Exception
+
+    public static String[] get() throws Exception
     {
         // URL url = new URL ("http://127.0.0.1:8000/api/movieTimingsapi/1/2018-03-15/?format=json");
         URL url = new URL ("http://127.0.0.1:8000/api/whatsonapi?format=json");
@@ -22,31 +21,56 @@ public class APIConnection{
         BufferedReader in = new BufferedReader(
 		        new InputStreamReader(con.getInputStream()));
 		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
+        inputLine = in.readLine();
 		in.close();
 
+        // deleting the []
+        inputLine = inputLine.replace("[","");
+        inputLine = inputLine.replace("]","");
+
 		//print result
-		System.out.println(response.toString());
+		//System.out.println(inputLine);
+
+        String[] separated = inputLine.split("[{}]");
+        int sLength = separated.length;
+        int jLength = sLength - (sLength/2);
+        String[] jsons = new String[jLength];
+        int j = 0;
+        for (int i = 0; i < sLength; i++)
+        {
+            if (!separated[i].equals(",") && !separated[i].equals(""))
+            {
+                String toAdd = "{";
+                jsons[j] = toAdd;
+                jsons[j] += separated[i];
+                jsons[j] += "}";
+                j++;
+            }
+        }
+
+        return jsons;
     }
 
     public static void main(String[] args)
     {
         try
         {
-            // APIConnection.get();
-            // String json = "{\"id\":1,\"title\":\"The Greates Showman\",\"cover\":\"img/MovieCovers/TheGreatestShowman.jpg\",\"rating\":8.0,\"certificate\":\"0\",\"releaseDate\":\"2018-02-18\"}";
-            String json = "{\"id\":1, \"title\": \"hello\"}";
-            // Gson gson = new Gson();
-            // Movie movie = gson.fromJson(json, Movie.class);
-            // Movie movie = new Movie(3, "Hey There!");
-            // System.out.println(movie.getTitle());
             ObjectMapper mapper = new ObjectMapper();
-            Movie movie = mapper.readValue(json, Movie.class);
-            System.out.println(movie.getTitle());
+            String[] jsons = APIConnection.get();
+            ArrayList<Movie> movies = new ArrayList<>();
+            for (int i = 0; i < jsons.length; i++)
+            {
+                System.out.println(jsons[i]);
+                Movie movie = mapper.readValue(jsons[i], Movie.class);
+                movies.add(movie);
+            }
+            System.out.println();
+            System.out.println("=== From ArrayList ===");
+            System.out.println();
+            for (int j = 0; j < movies.size(); j++)
+            {
+                System.out.println(movies.get(j).getTitle());
+            }
         }
         catch(Exception e)
         {
@@ -56,35 +80,44 @@ public class APIConnection{
 }
 
 
-// Small version of Movie for testing
 class Movie {
-    protected int id = 0;
-    protected String title = "";
+    private String title = "";
+    private int id = 0;
+    private String cover = "";
+    private double rating = 0;
+    private String releaseDate = "";
+    private String certificate = "";
 
+    public Movie(){}
 
-    public Movie()
-    {
-        this.id = 0;
-        this.title = null;
-    }
-
-    public Movie(int id, String title)
+    public Movie(int id, String title, String cover, double rating, String certificate, String releaseDate)
     {
         this.id = id;
         this.title = title;
     }
+
+    public int geID() { return id;}
 
     public String getTitle() { return title; }
 
-    public int getID() { return id;}
+    public String getCover() { return cover; }
 
-    public void setTitle(String title)
-    {
-        this.title = title;
-    }
+    public double getRating() { return rating; }
 
-    public void setID(int id)
-    {
-        this.id = id;
-    }
+    public String getCertificate() { return certificate; }
+
+    public String getReleaseDate() { return releaseDate; }
+
+    public void setID(int id) { this.id = id; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public void setCover(String cover) { this.cover = cover; }
+
+    public void setRating(double rating) { this.rating = rating; }
+
+    public void setCertificate(String certificate) { this.certificate = certificate; }
+
+    public void setReleaseDate(String releaseDate) { this.releaseDate = releaseDate; }
+
 }
