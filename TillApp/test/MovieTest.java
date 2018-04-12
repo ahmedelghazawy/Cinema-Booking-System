411d3a2f012033c7a984835ba7ad5a2cd5d9da3a
Ahmed El Ghazawy <sc16ameg@leeds.ac.uk>
2018-04-12 15:28:35 +0100

    created another version of APIConnection class for unit testing and a version of MovieTest for trial, but it's still not fully working yet

--- TillApp/test/MovieTest.java
+++ TillApp/test/MovieTest.java
@@ -1,8 +1,24 @@
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 /**
  * Created by sc16tdad on 09/02/18.
  */
 public class MovieTest {
+  private ArrayList<Movie> movies;
 
+  @Before
+  public void setup(){
+    movies = Movie.getMovies();
+  }
+
+  @Test
+  public void getMoviesTest(){
+    ArrayList<Movie> trialMovies= new ArrayList<>();
+    trialMovies.add(new Movie(1,"hello", "", 0.0, "", ""));
+    assertThat(movies, is(trialMovies));
+  }
 }
