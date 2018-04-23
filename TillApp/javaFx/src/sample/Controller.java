d27ef1bf6bbd1024b613fb181007933a28a66d50
Selim irez <sc16si@leeds.ac.uk>
2018-04-23 17:46:11 +0100

    Ticket quantity incrementation comlete

--- TillApp/javaFx/src/sample/Controller.java
+++ TillApp/javaFx/src/sample/Controller.java
@@ -1,37 +1,76 @@
 package sample;
 
-//import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
-import javafx.scene.text.Text;
-
-
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene; // what changes within the stage - backed by a scene graph - contains the UI nodes - scenes make it easier to use a container
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage; // top level UI container in JavaFX - the window frame itself
-
-import javax.swing.*;
-import java.awt.Window;
-
-
+import javafx.scene.control.Button;
 import javafx.scene.Node;
-import javafx.stage.Stage;
-
-
 import java.io.*;
+
 
 
 public class Controller {
 
 
+    int standardAdultQuantity = 0;
+    int vipAdultQuantity = 0;
+    int standardStudentQuantity = 0;
+    int vipStudentQuantity = 0;
+    int standardChildQuantity = 0;
+    int vipChildQuantity = 0;
+
     @FXML
     private Parent second;
     private Stage window;
+
+    @FXML
+    private Button plusButton1;
+    @FXML
+    private TextField textField1;
+    @FXML
+    private Button minusButton1;
+
+    @FXML
+    private Button plusButton2;
+    @FXML
+    private TextField textField2;
+    @FXML
+    private Button minusButton2;
+
+    @FXML
+    private Button plusButton3;
+    @FXML
+    private TextField textField3;
+    @FXML
+    private Button minusButton3;
+
+    @FXML
+    private Button plusButton4;
+    @FXML
+    private TextField textField4;
+    @FXML
+    private Button minusButton4;
+
+    @FXML
+    private Button plusButton5;
+    @FXML
+    private TextField textField5;
+    @FXML
+    private Button minusButton5;
+
+    @FXML
+    private Button plusButton6;
+    @FXML
+    private TextField textField6;
+    @FXML
+    private Button minusButton6;
+
+
 
 
     @FXML
@@ -91,5 +130,94 @@ public class Controller {
     public void seatSelected(ActionEvent event) throws IOException {
 
     }
+
+
+
+
+
+    @FXML
+    public void standardAdult(ActionEvent event){
+
+        if(event.getSource().equals(plusButton1)) {
+            standardAdultQuantity += 1;
+        } else if( standardAdultQuantity > 0 && event.getSource().equals(minusButton1)) {
+            standardAdultQuantity -= 1;
+        }
+
+        textField1.setText(String.valueOf(standardAdultQuantity));
+
+    }
+
+    @FXML
+    public void vipAdult(ActionEvent event){
+
+        if(event.getSource().equals(plusButton2)) {
+            vipAdultQuantity += 1;
+        } else if( vipAdultQuantity > 0 && event.getSource().equals(minusButton2)) {
+            vipAdultQuantity -= 1;
+        }
+
+        textField2.setText(String.valueOf(vipAdultQuantity));
+
+    }
+
+
+    @FXML
+    public void standardStudent(ActionEvent event){
+
+        if(event.getSource().equals(plusButton3)) {
+            standardStudentQuantity += 1;
+        } else if( standardStudentQuantity > 0 && event.getSource().equals(minusButton3)) {
+            standardStudentQuantity -= 1;
+        }
+
+        textField3.setText(String.valueOf(standardStudentQuantity));
+
+    }
+
+    @FXML
+    public void vipStudent(ActionEvent event){
+
+        if(event.getSource().equals(plusButton4)) {
+            vipStudentQuantity += 1;
+        } else if( vipStudentQuantity > 0 && event.getSource().equals(minusButton4)) {
+            vipStudentQuantity -= 1;
+        }
+
+        textField4.setText(String.valueOf(vipStudentQuantity));
+
+    }
+
+
+    @FXML
+    public void standardChild(ActionEvent event){
+
+        if(event.getSource().equals(plusButton5)) {
+            standardChildQuantity += 1;
+        } else if( standardChildQuantity > 0 && event.getSource().equals(minusButton5)) {
+            standardChildQuantity -= 1;
+        }
+
+        textField5.setText(String.valueOf(standardChildQuantity));
+
+    }
+
+
+    @FXML
+    public void vipChild(ActionEvent event){
+
+        if(event.getSource().equals(plusButton6)) {
+            vipChildQuantity += 1;
+        } else if( vipChildQuantity > 0 && event.getSource().equals(minusButton6)) {
+            vipChildQuantity -= 1;
+        }
+
+        textField6.setText(String.valueOf(vipChildQuantity));
+
+    }
+
+
+    
+
 
 }
