b64707c565661683982435fa22ed088f4495dc86
Selim irez <sc16si@leeds.ac.uk>
2018-04-26 06:41:23 +0100

    Ticket pages Commplete

--- TillApp/src/sample/Controller.java
+++ TillApp/src/sample/Controller.java
@@ -237,6 +237,16 @@ public class Controller {
 
     }
 
+    @FXML
+    public void moveToTicketPatelKiPunjabiShaadi(ActionEvent event) throws IOException {
+
+        Parent ticket = FXMLLoader.load(getClass().getResource("ticketTypePatelKiPunjabiShaadi.fxml"));
+        Scene ticketScene = new Scene(ticket, 1165, 700);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(ticketScene);
+
+    }
+
 
 
     @FXML
